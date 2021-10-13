0fbbf1dae360f023544b625cf92d6b44f0ea6b2c
EkaterinaM1313 <92455903+EkaterinaM1313@users.noreply.github.com>
2021-10-13 21:53:06 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ public static void main(String[] args) {
     String firstname = "Екатерина";
     String lastname = "Мешкова";
     int age = 39;
-    System.out.println(firstname+" " +lastname+" - "+age+" лет");
-    System.out.println("Москва");
+    String city = "Москва";
+    System.out.println(firstname+" " +lastname+" - "+age+" лет" +" " +city);
     }
 }
