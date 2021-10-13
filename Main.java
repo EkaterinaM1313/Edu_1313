68c9bbf71c513d0024aa913621039d9ef9ef4f9d
EkaterinaM1313 <92455903+EkaterinaM1313@users.noreply.github.com>
2021-10-13 21:37:40 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+public static void main(String[] args) {
+    String firstname = "Екатерина";
+    String lastname = "Мешкова";
+    int age = 39;
+    System.out.println(firstname+" " +lastname+" - "+age+" лет");
+    System.out.println("Москва");
+    }
+}
