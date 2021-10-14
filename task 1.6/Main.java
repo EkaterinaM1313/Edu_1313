b3f0575b9ed56a4356b7350633cad7160f291038
EkaterinaM1313 <92455903+EkaterinaM1313@users.noreply.github.com>
2021-10-14 16:16:27 +0300

    Create Main.java

---
+++ task 1.6/Main.java
@@ -0,0 +1,10 @@
+      import java.util.Scanner;
+        public class Main {
+            public static void main(String[] args) {
+                Scanner scan = new Scanner(System.in);
+                int X = scan.nextInt();
+                double num1 =0.15;
+                double num2 = num1 * X;
+                System.out.println(num2+X);
+            }
+        }
