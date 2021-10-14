b26bf6d8fdbe8876e52e3331387d1ed06d50be77
EkaterinaM1313 <92455903+EkaterinaM1313@users.noreply.github.com>
2021-10-14 14:55:34 +0300

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-public static void main(String[] args) {
-    String firstname = "Екатерина";
-    String lastname = "Мешкова";
-    int age = 39;
-    String city = "Москва";
-    System.out.println(firstname+" " +lastname+" - "+age+" лет" +" " +city);
-    }
-}
