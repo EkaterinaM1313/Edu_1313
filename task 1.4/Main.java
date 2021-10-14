fe74de1beb673455f74b086e01e66fef6387fd51
EkaterinaM1313 <92455903+EkaterinaM1313@users.noreply.github.com>
2021-10-14 14:54:21 +0300

    Create Main.java

---
+++ task 1.4/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+    // Возведение в квадрат числа 25
+    int num1=25;
+    int num2=2;
+    System.out.println("Число 25 в квадрате равно " + Math.pow(num1,num2));
+    }
+}
