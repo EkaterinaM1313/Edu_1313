f0b5c5294de3af120401a525022d82cb56386a61
EkaterinaM1313 <92455903+EkaterinaM1313@users.noreply.github.com>
2021-10-14 14:08:07 +0300

    Create Main.java

---
+++ task 1.3/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Добро пожаловать в команду!");
+        int x, y;
+        x = 264;
+        y = 1;
+        System.out.println(x);
+        System.out.println(y);
+        }
+}
