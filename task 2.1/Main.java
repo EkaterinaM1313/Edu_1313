85a154e7aeb6e4df8d5a9e4735cd01e394118ce6
EkaterinaM1313 <92455903+EkaterinaM1313@users.noreply.github.com>
2021-10-15 15:50:02 +0300

    Create Main.java

---
+++ task 2.1/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] arg) {
+        String a = "Мама";
+        String b = "Мыла";
+        String c = "Раму";
+        System.out.println(a+b+c);
+        System.out.println(a+c+b);
+        System.out.println(b+a+c);
+        System.out.println(c+a+b);
+        System.out.println(b+c+a);
+    }
+}
