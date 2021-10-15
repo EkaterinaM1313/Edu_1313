9ba6d024743523b1e743ae5dfad5d3a0170dd976
EkaterinaM1313 <92455903+EkaterinaM1313@users.noreply.github.com>
2021-10-15 16:34:00 +0300

    Create Main.java

---
+++ task 2.3/Main.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Solution {
+             public static void main(String[] agrs) throws Exception {
+            String a = "8";
+            for (int i = 1; i <= 10; i++) {
+                for (int j = 1; j <= i; j++) {
+                    System.out.print(a);
+                }
+                System.out.println("");
+            }
+        }
+    }
