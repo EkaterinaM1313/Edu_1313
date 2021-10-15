1c71a68ddc3a97b32bdf28801f0e71090207e8da
EkaterinaM1313 <92455903+EkaterinaM1313@users.noreply.github.com>
2021-10-15 13:50:19 +0300

    Create main.java

---
+++ lesson/main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String [] args) {
+        String [] cars = {"BMW", "Audi", "Kia"};
+        System.out.println(cars[0]);
+        System.out.println(cars[1]);
+        System.out.println(cars[2]);
+        System.out.println(cars.length);
+        System.out.println(cars[3]);
+    }
+}
