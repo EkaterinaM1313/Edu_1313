5855f72e9ed115e936dd7f78669d976719f0edcc
EkaterinaM1313 <92455903+EkaterinaM1313@users.noreply.github.com>
2021-10-17 13:07:37 +0300

    Create Main.java

---
+++ task 2.6/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        int a,b,c;
+        Scanner in = new Scanner(System.in);
+        a = in.nextInt();
+        b = in.nextInt();
+        c=a%b;
+        System.out.println("Делится с остатком: " +c);
+    }
+}
