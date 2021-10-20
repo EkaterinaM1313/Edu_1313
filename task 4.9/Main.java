0dff4bc869be076aae4e25829b143005f482feea
EkaterinaM1313 <92455903+EkaterinaM1313@users.noreply.github.com>
2021-10-20 20:12:13 +0300

    Create Main.java

---
+++ task 4.9/Main.java
@@ -0,0 +1,61 @@
+public class Main {
+    public static void main(String[] args) {
+        Person person8 = new Person("Серафим", "Иванов", 75, null, null);
+        Person person7 = new Person("Авдотья","Сидорова", 65, null, null);
+        Person person2 = new Person("Федор", "Сидоров", 61, null, null);
+        Person person3 = new Person("Клава", "Иванова", 59, null, null);
+        Person person4 = new Person("Иван", "Иванов", 30, person3, person8);
+        Person person5 = new Person("Ева", "Иванова", 30, person7, person2);
+        Person person6 = new Person("Филипп", "Иванов", 7, person5, person4);
+        person7.info();
+            }
+}
+
+class Person{
+    private String name;
+    private String lastname;
+    private int age;
+    private int hp;
+    private Person mother;
+    private Person father;
+    Person(String name, String lastname, int age, Person mother, Person father){
+        this.name = name;
+        this.lastname = lastname;
+        this.age = age;
+        this.hp = 100;
+        this.mother = mother;
+        this.father = father;
+    }
+    void sayHello(String name){
+        System.out.println("Привет, "+name+" меня зовут "+this.name);
+    }
+    public void info(){
+        String info = "Меня зовут "+this.name+"\n";
+
+        if (this.getMother() != null){
+            info += "Мою маму зовут "+this.getMother().name+"\n";
+            if(this.getMother().getFather() != null){
+                info += "Моего дедушку по маминой линии зовут "+this.getMother().getFather().name+"\n";
+            }
+        }
+        if (this.getFather() != null){
+            info += "Моего папу зовут "+this.getFather().name+"\n";
+            if(this.getFather().getMother() != null){
+                info += "Мою бабушку по папиной линии зовут "+this.getFather().getMother().name+"\n";
+            }
+        }
+        System.out.println(info);
+    }
+
+    public Person getMother() {
+        return mother;
+    }
+
+    public Person getFather() {
+        return father;
+    }
+
+    public int getHp(){
+        return this.hp;
+    }
+    }
