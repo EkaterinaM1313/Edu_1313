d352efa034545c5f0010ac95a69ab88fac32dcb7
EkaterinaM1313 <92455903+EkaterinaM1313@users.noreply.github.com>
2021-10-21 16:57:13 +0300

    Update main.java

--- lesson/main.java
+++ lesson/main.java
@@ -1,12 +1,18 @@
-import java.util.Scanner;
-
 public class Main {
-    public static void main(String [] args) {
-        String [] cars = {"BMW", "Audi", "Kia"};
-        System.out.println(cars[0]);
-        System.out.println(cars[1]);
-        System.out.println(cars[2]);
-        System.out.println(cars.length);
-        System.out.println(cars[3]);
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        System.out.println("Кличка собаки - "+dog.getName()+" \n Возраст собаки - "+dog.getAge());
+        dog.setName("Наполеон");
+        dog.setAge(5);
+        System.out.println("Новая кличка собаки - "+dog.getName()+" \n Новый возраст собаки - "+dog.getAge());
     }
+}
+
+class Dog {
+    private String name = "Купе";
+    private int age = 6;
+    public String getName() {return name; }
+    public void setName(String name) { this.name = name; }
+    public int getAge() { return age; }
+    public void setAge(int age) { this.age = age; }
 }
